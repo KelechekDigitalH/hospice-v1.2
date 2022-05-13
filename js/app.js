8fa70b35c9e40570ab0c27d438acaeae4aaf356f
Myrzaiym Askarova <myrzaiym.askarova.kk@gmail.com>
2022-05-13 23:27:50 +0600

    feat: update breadcrumbs  page legal-entity and donation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /*******Burger******/
-const mainBg = document.querySelector(".main");
+
 
 document.querySelector('.burger').addEventListener('click', function(){
-	mainBg.classList.toggle("main-bg");
+
 	document.querySelector('.burger','.menu').classList.toggle('active');
 	document.querySelector('.menu').classList.toggle("open");
  });
