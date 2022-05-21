dbd2fa0ea65e06d33a64911b30a661de8471a159
Azamat Zhamankul <azamat.zhamankulov.kk@gmail.com>
2022-05-21 23:19:07 +0600

    pulling form dev

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 /*******Burger******/
 
-document.querySelector('.burger').addEventListener('click', function() {
-	document.querySelector('.burger','.menu').classList.toggle('active');
+document.querySelector('.burger').addEventListener('click', function () {
+	document.querySelector('.burger', '.menu').classList.toggle('active');
 	document.querySelector('.menu').classList.toggle("open");
- });
- 
- // END BURGER SCRIPTS
+});
+
+// END BURGER SCRIPTS
 
 
 // NEED HELP PAGE SCRIPTS
@@ -79,66 +79,65 @@ accordionEntity.forEach(accordionEntitys => {
 // INDIVIDUALS PAGE SCRIPTS
 // window.forEach(window => {
 window.addEventListener("load", function () {
-    const btn = document.querySelector(".individuals_link");
-    const window = document.querySelector(".btn-modal");
-    const windowClose = document.querySelector(".off");
-  
-    btn.addEventListener("click", function () {
-      window.classList.toggle("active");
-    });
-  
-    windowClose.addEventListener("click", function () {
-      window.classList.remove("active");
+	const btn = document.querySelector(".individuals_link");
+	const window = document.querySelector(".btn-modal");
+	const windowClose = document.querySelector(".off");
 
-    });
-  });
+	btn.addEventListener("click", function () {
+		window.classList.toggle("active");
+	});
 
+	windowClose.addEventListener("click", function () {
+		window.classList.remove("active");
 
-  window.addEventListener("load", function () {
-    const btn2 = document.querySelector(".individuals_link2");
-    const window2 = document.querySelector(".btn-modal2");
-    const windowClose2 = document.querySelector(".off2");
-  
-    btn2.addEventListener("click", function () {
-      window2.classList.toggle("active");
-    });
-  
-    windowClose2.addEventListener("click", function () {
-      window2.classList.remove("active");
-
-    });
-  });
+	});
+});
 
 
-  window.addEventListener("load", function () {
-    const btn3 = document.querySelector(".individuals_link3");
-    const window3 = document.querySelector(".btn-modal3");
-    const windowClose3 = document.querySelector(".off3");
-  
-    btn3.addEventListener("click", function () {
-      window3.classList.toggle("active");
-    });
-  
-    windowClose3.addEventListener("click", function () {
-      window3.classList.remove("active");
+window.addEventListener("load", function () {
+	const btn2 = document.querySelector(".individuals_link2");
+	const window2 = document.querySelector(".btn-modal2");
+	const windowClose2 = document.querySelector(".off2");
 
-    });
-  });
+	btn2.addEventListener("click", function () {
+		window2.classList.toggle("active");
+	});
 
-  window.addEventListener("load", function () {
-    const btn4 = document.querySelector(".individuals_link4");
-    const window4 = document.querySelector(".btn-modal4");
-    const windowClose4 = document.querySelector(".off4");
-  
-    btn4.addEventListener("click", function () {
-      window4.classList.toggle("active");
-    });
-  
-    windowClose4.addEventListener("click", function () {
-      window4.classList.remove("active");
+	windowClose2.addEventListener("click", function () {
+		window2.classList.remove("active");
 
-    });
-  });
+	});
+});
+
+window.addEventListener("load", function () {
+	const btn3 = document.querySelector(".individuals_link3");
+	const window3 = document.querySelector(".btn-modal3");
+	const windowClose3 = document.querySelector(".off3");
+
+	btn3.addEventListener("click", function () {
+		window3.classList.toggle("active");
+	});
+
+	windowClose3.addEventListener("click", function () {
+		window3.classList.remove("active");
+
+	});
+});
+
+window.addEventListener("load", function () {
+	const btn4 = document.querySelector(".individuals_link4");
+	const window4 = document.querySelector(".btn-modal4");
+	const windowClose4 = document.querySelector(".off4");
+
+	btn4.addEventListener("click", function () {
+		window4.classList.toggle("active");
+	});
+
+	windowClose4.addEventListener("click", function () {
+		window4.classList.remove("active");
+
+	});
+});
 // });
 
 
@@ -152,12 +151,12 @@ var swiper = new Swiper(".mySwiper", {
 	loop: true,
 	loopFillGroupWithBlank: true,
 	pagination: {
-	   el: ".swiper-pagination",
-	   clickable: true,
+		el: ".swiper-pagination",
+		clickable: true,
 	},
 	navigation: {
-	   nextEl: ".swiper-button-next",
-	   prevEl: ".swiper-button-prev",
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
 	},
 	breakpoints: {
 		300: {
@@ -193,7 +192,7 @@ var swiper = new Swiper(".mySwiper", {
 			spaceBetween: 30
 		}
 	}
- });
+});
 
 
 //slider mainpage our-sponsors
